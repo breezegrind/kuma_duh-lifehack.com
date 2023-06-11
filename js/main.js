1137c0ee31783f0730a8d9f58cfef6355ca606d8
bear <breezegrind@nifty.com>
2023-06-12 02:19:19 +0900

    good

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function($){
+    $(".modal-button").modaal({
+        hide_close:true,
+        overlay_close:true,
+        animation_speed:100,
+        width: 1100,
+        height: 610,
+        background_scroll:true
+    })  
+})
